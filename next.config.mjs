e4ed084fdc4794425c6033a90223213afe6ccc55
Alexandr Kalashnikov <159815449+sonyaflexx@users.noreply.github.com>
2024-11-06 01:24:07 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: async () => [{
+    source: '/:path*',
+    headers: [{
+      key: 'Cache-Control',
+      value: 'no-store, max-age=0',
+    }],
+  }],
+}
 
 export default nextConfig;
