b75571d7181d77bc300ad845216bacd9b16120e3
Alexandr Kalashnikov <159815449+sonyaflexx@users.noreply.github.com>
2024-11-06 01:27:20 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   headers: async () => [{
     source: '/:path*',
-    headers: [{
-      key: 'Cache-Control',
-      value: 'no-store, max-age=0',
-    }],
+    headers: [
+      {
+        key: 'Cache-Control',
+        value: 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+      },
+      {
+        key: 'Pragma',
+        value: 'no-cache',
+      },
+      {
+        key: 'Expires',
+        value: '0',
+      },
+    ],
   }],
+  experimental: {
+    staticPageGenerationTimeout: 1000,
+  },
 }
 
 export default nextConfig;
